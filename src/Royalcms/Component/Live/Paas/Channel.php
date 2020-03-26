5ef4bb42da54a3d46c445e771c0a61ae712c1cf0
王正东 <wangzhengdong@ecmoban.com>
2020-03-26 12:09:14 +0800

    添加Pass SDK类

---
+++ src/Royalcms/Component/Live/Paas/Channel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Royalcms\Component\Live\Paas;
+
+/**
+ * @file Channel.php
+ * @author Yan.Gao
+ * @version 0.0.1
+ * @date 2018-04-20
+ */
+
+class Channel extends Common
+{
+
+}
