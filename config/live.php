0e4ec4848f3e300193b6237a578116daf3683172
王正东 <wangzhengdong@ecmoban.com>
2020-03-26 12:08:22 +0800

    更新配置文件

---
+++ config/live.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /**
+     * APPID
+     * 控制台中获取
+     */
+    'app_id'        => 'xxxx',
+
+    /**
+     * 应用密钥
+     * 控制台中获取
+     */
+    'secret_key'    => 'xxx',
+
+    /**
+     * 是否显示构造请求连接&参数 json console (请勿在生产环境打开)
+     */
+    'show_request_url' => false,
+
+    /**
+     * 是否显示接口返回数据 json console (请勿在生产环境打开)
+     */
+    'show_request_data' => false,
+
+
+];
